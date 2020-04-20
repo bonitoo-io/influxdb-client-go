355305ff1f7b3ede3b5a311bb0180c479bfff4b7
vlastahajek <vlastimil.hajek@bonitoo.io>
2020-04-20 19:26:28 +0200

    feat: added benchmarking client

---
+++ cmd/main.go
@@ -0,0 +1,256 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"github.com/influxdata/influxdb-client-go"
+	"log"
+	"os"
+	"runtime"
+	"runtime/pprof"
+	"sync"
+	"time"
+)
+
+const (
+	InfluxDB2Url    = "http://localhost:9999"
+	InfluxDB2Bucket = "my-bucket"
+	InfluxDB2Org    = "my-org"
+)
+
+type Writer interface {
+	Write(id int, measurementName string, iteration int)
+	Count(measurementName string) (int, error)
+	Close() error
+}
+
+type WriterV2P struct {
+	influx   influxdb2.InfluxDBClient
+	writeApi influxdb2.WriteApi
+}
+type WriterV2R struct {
+	influx   influxdb2.InfluxDBClient
+	writeApi influxdb2.WriteApi
+}
+
+func main() {
+	batchSize := flag.Uint("batchSize", 1000, "batch size")
+	writerType := flag.String("type", "lines", "Type of writer (lines, points)")
+	threadsCount := flag.Int("threadsCount", 2000, "how much Thread use to write into InfluxDB")
+	secondsCount := flag.Int("secondsCount", 30, "how long write into InfluxDB")
+	lineProtocolsCount := flag.Int("lineProtocolsCount", 100, "how much data writes in one batch")
+	skipCount := flag.Bool("skipCount", false, "skip counting count")
+	token := flag.String("token", "my-token", "token")
+	measurementName := flag.String("measurementName", fmt.Sprintf("sensor_%d", time.Now().UnixNano()), "writer measure destination")
+	debugLevel := flag.Uint("debugLevel", 0, "Log messages level: 0 - error, 1 - warning, 2 - info, 3 - debug")
+	cpuprofile := flag.String("cpuprofile", "", "write cpu profile to `file`")
+	memprofile := flag.String("memprofile", "", "write memory profile to `file`")
+
+	flag.Parse()
+
+	if *token == "" {
+		fmt.Println("Token must be specified")
+		return
+	}
+
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
+	expected := (*threadsCount) * (*secondsCount) * (*lineProtocolsCount)
+
+	//green := color.New(color.FgHiGreen).SprintFunc()
+	fmt.Println()
+	fmt.Printf("------------- %s -------------", *writerType)
+	fmt.Println()
+	fmt.Println()
+	fmt.Println("measurement:        ", *measurementName)
+	fmt.Println("threadsCount:       ", *threadsCount)
+	fmt.Println("secondsCount:       ", *secondsCount)
+	fmt.Println("lineProtocolsCount: ", *lineProtocolsCount)
+	fmt.Println()
+	fmt.Println("expected size: ", expected)
+	fmt.Println()
+
+	var writer Writer
+	influx := influxdb2.NewClientWithOptions(InfluxDB2Url, *token, influxdb2.DefaultOptions().
+		SetBatchSize(*batchSize).
+		SetLogLevel(*debugLevel).
+		SetRetryInterval(1000).
+		SetFlushInterval(1000))
+	if *writerType == "points" {
+		writer = &WriterV2P{influx: influx, writeApi: influx.WriteApi(InfluxDB2Org, InfluxDB2Bucket)}
+	} else if *writerType == "lines" {
+		writer = &WriterV2R{influx: influx, writeApi: influx.WriteApi(InfluxDB2Org, InfluxDB2Bucket)}
+	} else {
+		fmt.Printf("unsupported client %s\n", *writerType)
+		return
+	}
+
+	stopExecution := make(chan bool)
+	var wg sync.WaitGroup
+	wg.Add(*threadsCount)
+
+	start := time.Now()
+
+	for i := 1; i <= *threadsCount; i++ {
+		go doLoad(&wg, stopExecution, i, *measurementName, *secondsCount, *lineProtocolsCount, writer)
+	}
+
+	go func() {
+		time.Sleep(time.Duration(*secondsCount) * time.Second)
+		fmt.Printf("\n\nThe time: %v seconds elapsed! Stopping all writers\n\n", *secondsCount)
+		close(stopExecution)
+	}()
+
+	wg.Wait()
+
+	if !*skipCount {
+		fmt.Println()
+		fmt.Println()
+		fmt.Println("Querying InfluxDB ...")
+		fmt.Println()
+
+		total, err := writer.Count(*measurementName)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Results:")
+		fmt.Println("-> expected:        ", expected)
+		fmt.Println("-> total:           ", total)
+		fmt.Println("-> rate [%]:        ", (float64(total)/float64(expected))*100)
+		fmt.Println("-> rate [msg/sec]:  ", total / *secondsCount)
+		fmt.Println()
+		fmt.Println("Total time:", time.Since(start))
+	}
+
+	if err := writer.Close(); err != nil {
+		panic(err)
+	}
+
+	if *memprofile != "" {
+		f, err := os.Create(*memprofile)
+		if err != nil {
+			log.Fatal("could not create memory profile: ", err)
+		}
+		runtime.GC() // get up-to-date statistics
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			log.Fatal("could not write memory profile: ", err)
+		}
+		f.Close()
+	}
+}
+
+func doLoad(wg *sync.WaitGroup, stopExecution <-chan bool, id int, measurementName string, secondsCount int, lineProtocolsCount int, influx Writer) {
+	defer wg.Done()
+
+	for i := 1; i <= secondsCount; i++ {
+		select {
+		case <-stopExecution:
+			return
+		default:
+
+			if id == 1 {
+				fmt.Printf("\rwriting iterations: %v/%v", i, secondsCount)
+			}
+
+			start := i * lineProtocolsCount
+			end := start + lineProtocolsCount
+			for j := start; j < end; j++ {
+				select {
+				case <-stopExecution:
+					return
+				default:
+					influx.Write(id, measurementName, j)
+				}
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}
+}
+
+func (w *WriterV2R) Write(id int, measurementName string, iteration int) {
+	record := fmt.Sprintf("%s,id=%d temperature=%d %d", measurementName, id, time.Now().UnixNano(), time.Unix(0, int64(iteration)).UnixNano())
+	w.writeApi.WriteRecord(record)
+	//fmt.Println(record)
+}
+
+func (w *WriterV2R) Count(measurementName string) (int, error) {
+	query := `from(bucket:"` + InfluxDB2Bucket + `") 
+		|> range(start: 0, stop: now()) 
+		|> filter(fn: (r) => r._measurement == "` + measurementName + `") 
+		|> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
+		|> drop(columns: ["id", "host"])
+		|> count(column: "temperature")`
+
+	queryApi := w.influx.QueryApi(InfluxDB2Org)
+	queryResult, err := queryApi.Query(context.Background(), query)
+	if err != nil {
+		return 0, err
+	}
+	total := 0
+	if !queryResult.Next() {
+		if queryResult.Err() != nil {
+			return 0, queryResult.Err()
+		} else {
+			return 0, errors.New("unknown error")
+		}
+	} else {
+		total = int(queryResult.Record().ValueByKey("temperature").(int64))
+	}
+	return total, nil
+}
+
+func (w *WriterV2R) Close() error {
+	w.influx.Close()
+	return nil
+}
+
+func (w *WriterV2P) Write(id int, measurementName string, iteration int) {
+	point := influxdb2.NewPoint(
+		measurementName,
+		map[string]string{"id": fmt.Sprintf("%v", id)},
+		map[string]interface{}{"temperature": fmt.Sprintf("%v", time.Now().UnixNano())},
+		time.Unix(0, int64(iteration)))
+	w.writeApi.WritePoint(point)
+}
+
+func (w *WriterV2P) Count(measurementName string) (int, error) {
+	query := `from(bucket:"` + InfluxDB2Bucket + `") 
+		|> range(start: 0, stop: now()) 
+		|> filter(fn: (r) => r._measurement == "` + measurementName + `") 
+		|> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
+		|> drop(columns: ["id", "host"])
+		|> count(column: "temperature")`
+
+	queryApi := w.influx.QueryApi(InfluxDB2Org)
+	queryResult, err := queryApi.Query(context.Background(), query)
+	if err != nil {
+		return 0, err
+	}
+	total := 0
+	if !queryResult.Next() {
+		if queryResult.Err() != nil {
+			return 0, queryResult.Err()
+		} else {
+			return 0, errors.New("unknown error")
+		}
+	} else {
+		total = int(queryResult.Record().ValueByKey("temperature").(int64))
+	}
+	return total, nil
+}
+
+func (w *WriterV2P) Close() error {
+	w.influx.Close()
+	return nil
+}
