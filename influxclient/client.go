7cbd6c35517f4dda8678881a365515ed594161c9
vlastahajek <vlastimil.hajek@bonitoo.io>
2021-01-19 14:41:14 +0100

    doc: adding package docs

--- influxclient/client.go
+++ influxclient/client.go
@@ -1,3 +1,8 @@
+// Copyright 2020 InfluxData, Inc. All rights reserved.
+// Use of this source code is governed by MIT
+// license that can be found in the LICENSE file.
+
+// Package influxclient provides client for InfluxDB server.
 package influxclient
 
 import (
